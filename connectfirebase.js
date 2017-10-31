633e20bb3f15caeac52a716a5a7c02ee965c93be
DESKTOP-D7JVEFC\onasu <filmzaza23@gmail.com>
2017-10-31 16:27:52 +0700

    add login

--- connectfirebase.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    apiKey: "AIzaSyB2_l8HIycLAdM6a9vA9XMaS_VPqxmBK1o",
-    authDomain: "datacontro.firebaseapp.com",
-    databaseURL: "https://datacontro.firebaseio.com",
-    projectId: "datacontro",
-    storageBucket: "datacontro.appspot.com",
-    messagingSenderId: "585589132177"
-};
-firebase.initializeApp(config);
